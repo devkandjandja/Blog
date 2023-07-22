c7d2c2e902072a29fec484bdbb66999d93a9f65f
devkandjandja <83196901+devkandjandja@users.noreply.github.com>
2023-07-22 14:49:59 +0100

    melhorando dados de usuario

--- Screens/UserScreens/DeleteUserScreens.cs
+++ Screens/UserScreens/DeleteUserScreens.cs
@@ -1,15 +1,39 @@
+using Blog.Models;
+using Blog.Repositories;
+
 namespace Blog.Screens.UserScreens
 {
     public static class DeleteUserScreens
     {
         public static void Load()
         {
+            Console.Clear();
+            Console.WriteLine("Seleciona o id para excluir");
+            Console.WriteLine("---------------------");
+
+            Console.WriteLine("Id: ");
+            var id = Console.ReadLine();
             
+
+            Delete(int.Parse(id)); 
+
+            Console.ReadKey();
+            MenuUserScreens.Load();
         }
 
-        public static void Delete()
+        public static void Delete(int id)
         {
-
+            try
+            {
+                var repositorio = new Repositorio<User>(Database.Connection);
+                repositorio.Delete(id);
+                Console.WriteLine("Usuario cadastrado com sucesso!");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Erro ao excluir usuario!");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
